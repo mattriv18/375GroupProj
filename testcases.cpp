d341dda24df01d3301c8ce9b7af617799dbe0923
nstecyna <glismaj@gmail.com>
2020-05-03 17:37:51 -0400

    added test case generator code

---
+++ testcases.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <stdlib.h>
+#include <fstream>
+ 
+using namespace std;
+ 
+// A function to generate random graph.
+void GenerateRandGraphs(long NOE, int NOV) {
+	long j, edge[NOE][2], count;
+	long i = 0;
+	// Build a connection between two random vertex.
+	while (i < NOE) {
+		edge[i][0] = (long) rand() % NOV + 1;
+		edge[i][1] = (long) rand() % NOV + 1;
+ 
+		if(edge[i][0] == edge[i][1])
+			continue;
+		else {
+			for(j = 0; j < i; j++) {
+				if((edge[i][0] == edge[j][0] && edge[i][1] == edge[j][1]) || (edge[i][0] == edge[j][1] && edge[i][1] == edge[j][0]))
+					i--;
+			}
+		}
+		i++;
+	}
+ 
+    ofstream outFile("output.txt");
+
+    i = 0;
+    while (i < NOE) {
+            outFile << edge[i][0] - 1 << " " << edge[i][1] - 1 << " " << 1 + rand() % 20 << endl;
+        i++;
+    }
+
+    outFile.close();
+
+    /*
+	// Print the random graph.
+	cout << "\nThe generated random random graph is: ";
+	for(i = 0; i < NOV; i++) {
+		count = 0;
+		cout << "\n\t" << i + 1 << "-> { ";
+		for(j = 0; j < NOE; j++) {
+			if (edge[j][0] == i + 1) {
+				cout << edge[j][1] << "   ";
+				count++;
+			} else if (edge[j][1] == i + 1) {
+				cout << edge[j][0] << "   ";
+				count++;
+			} else if (j == NOE - 1 && count == 0)
+				cout << "Isolated Vertex!";
+		    }
+		    cout << " }";
+	}	
+    */
+}
+ 
+int main()
+{
+	long n, i, e, v, max;
+ 
+	cout<<"Random graph generation: ";
+ 
+	// Randomly assign vertex and edges of the graph.
+	v = 1 + rand() % 99999;
+	cout << "\nThe graph has " << v << " vertexes.";
+
+    max = (((v * (v-1)) / 2) - 1);
+    if (max > RAND_MAX) max = RAND_MAX;
+	e = 1 + rand() % max;
+	cout << "\nThe graph has " << e << " edges.\n";
+ 
+	// A function to generate a random undirected graph with e edges and v vertexes.
+	GenerateRandGraphs(e, v);
+}
