b056c8c1261d14e1e137466079cce91e1ed36453
Nicholas Stecyna <nstecyn1@binghamton.edu>
2020-05-03 20:51:45 -0400

    fixed issues with dense graph generation

--- testcases.cpp
+++ testcases.cpp
@@ -21,33 +21,46 @@ void GenerateRandGraph(int NOE, int NOV, enum Type type) {
 
 	int i = 0;
 	// Build a connection between two random vertex.
-	while (i < NOE) {
-		edge[i][0] = rand() % NOV + 1;
-		edge[i][1] = rand() % NOV + 1;
-		string f_edgeStr = to_string(edge[i][0]) + to_string(edge[i][1]);
-		string r_edgeStr = to_string(edge[i][1]) + to_string(edge[i][0]);
-		cout << f_edgeStr << endl; 
-		if(edge[i][0] == edge[i][1]){
-			continue;
-		} else {
-			cout << f_edgeStr << endl;
-			try {
-				edgeCheck.at(f_edgeStr);
-				i--;
-			} catch (...) {
-				edgeCheck[f_edgeStr] = true;
-				edgeCheck[r_edgeStr] = true;
-			}
-		} /*else {
-			for(j = 0; j < i; j++) {
-				if((edge[i][0] == edge[j][0] && edge[i][1] == edge[j][1]) || (edge[i][0] == edge[j][1] && edge[i][1] == edge[j][0])){
-					i--;
-					break;
-				}
+	if (NOE == (NOV * (NOV-1) / 2) ) {
+		int e = 0;
+		for (i = 0; i < (NOV + 1); i++) {
+			for (j = i + 1; j < NOV; j++) {
+				if (i == j) continue;
+				edge[e][0] = i + 2;
+				edge[e][1] = j + 2;
+				e++;
 			}
 		}
-		*/
-		i++;
+	} else {
+		while (i < NOE) {
+			edge[i][0] = rand() % NOV + 1;
+			edge[i][1] = rand() % NOV + 1;
+			string f_edgeStr = to_string(edge[i][0]) + to_string(edge[i][1]);
+			string r_edgeStr = to_string(edge[i][1]) + to_string(edge[i][0]);
+			cout << f_edgeStr << endl; 
+			if(edge[i][0] == edge[i][1]){
+				continue;
+			} else {
+				cout << f_edgeStr << endl;
+				try {
+					edgeCheck.at(f_edgeStr);
+					i--;
+				} catch (...) {
+					edgeCheck[f_edgeStr] = true;
+					edgeCheck[r_edgeStr] = true;
+				}
+			} /*else {
+			    for(j = 0; j < i; j++) {
+			    if((edge[i][0] == edge[j][0] && edge[i][1] == edge[j][1]) || (edge[i][0] == edge[j][1] && edge[i][1] == edge[j][0])){
+			    i--;
+			    break;
+			    }
+			    }
+			    }
+			    */
+			i++;
+		}
+
 	}
 	string out;
 	switch(type){
@@ -55,35 +68,36 @@ void GenerateRandGraph(int NOE, int NOV, enum Type type) {
 			out = "./tests/random/test" + to_string(NOV) + "_" + "random";
 			break;
 		case DENSE:
-			out = "./test/dense/test" + to_string(NOV) + "_" + "dense";
+			out = "./tests/dense/test" + to_string(NOV) + "_" + "dense";
 			break;
 		case SPARSE:
 			out = "./tests/sparse/test" + to_string(NOV) + "_" + "sparse";
 	}
-    	ofstream outFile(out);
+	ofstream outFile(out);
 
-    	i = 0;
+	i = 0;
 	outFile << NOV << endl;
-    	while (i < NOE) {
-    	        outFile << edge[i][0] - 1 << " " << edge[i][1] - 1 << " " << 1 + rand() % 20 << endl;
-    	    i++;
-    	}
+	while (i < NOE) {
+		outFile << edge[i][0] - 1 << " " << edge[i][1] - 1 << " " << 1 + rand() % 20 << endl;
+		i++;
+	}
 
-    	outFile.close();
-    	for(int e = 0; e < NOE; e++){
+	outFile.close();
+	for(int e = 0; e < NOE; e++){
 		delete[] edge[e];
-    	}
-    	delete[] edge;
+	}
+	delete[] edge;
 }
- 
+
 int main(int argc, char* argv[])
 {
 	int e, v, max;
 	for(int i = 5; i <= 500; i++){
+		cout << "===================================\n";
 		v = i;
 		// Random Graph Generation
-		max = ((v * (v-1)) / 2) - 1;
-		e = v + rand() % max;
+		max = ((v * (v-1)) / 2);
+		e = v + rand() % (max - 1);
 		cout << "Generating Random Graph => " << "V: " << v << "E: " << e << endl;
 		GenerateRandGraph(e, v, RANDOM);
 
