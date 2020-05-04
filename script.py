3c80a765200cee54e518c67add634b833d23dc27
Matthew Rivera <mriver25@binghamton.edu>
2020-05-04 00:26:09 -0400

    test trial complete

--- script.py
+++ script.py
@@ -1,18 +1,28 @@
 import os
 
-# os.system("make")
+os.system("break>matrixtimes_sparse.txt")
+os.system("break>matrixtimes_dense.txt")
+os.system("break>matrixtimes_random.txt")
+os.system("break>heaptimes_sparse.txt")
+os.system("break>heaptimes_dense.txt")
+os.system("break>heaptimes_random.txt")
+os.system("break>lazyheaptimes_sparse.txt")
+os.system("break>lazyheaptimes_dense.txt")
+os.system("break>lazyheaptimes_random.txt")
 
 # matrix
-
-os.system("break>matrixtimes.txt")
-
+print("Running maxtrixDijkstras...")
 for i in range(5,501):
-    if (!path.exists("tests/sparse/test" + str(i) + "_sparse"))
+    if (not os.path.exists("tests/sparse/test" + str(i) + "_sparse")):
         continue
-    os.system("./matrixDijkstras tests/sparse/test" + str(i) + "_sparse >> matrixtimes_spare.txt")
+    print(i)
+    #print("Running maxtrixDijkstras sparse...");
+    os.system("./matrixDijkstras tests/sparse/test" + str(i) + "_sparse >> matrixtimes_sparse.txt")
 
+    #print("Running maxtrixDijkstras dense...");
     os.system("./matrixDijkstras tests/dense/test" + str(i) + "_dense >> matrixtimes_dense.txt")
 
+    #print("Running maxtrixDijkstras random...");
     os.system("./matrixDijkstras tests/random/test" + str(i) + "_random >> matrixtimes_random.txt")
 
 '''
@@ -29,24 +39,34 @@ for l in lines:
 '''
 
 # heap
+print("Running heapDijkstras...")
 for i in range(5,501):
-    if (!path.exists("tests/sparse/test" + str(i) + "_sparse"))
+    if (not os.path.exists("tests/sparse/test" + str(i) + "_sparse")):
         continue
-
+    print(i)
+    #print("Running heapDijkstras sparse...");
     os.system("./heapDijkstras tests/sparse/test" + str(i) + "_sparse >> heaptimes_sparse.txt")
 
+    #print("Running heapDijkstras dense...");
     os.system("./heapDijkstras tests/dense/test" + str(i) + "_dense >> heaptimes_dense.txt")
 
+    #print("Running heapDijkstras random...");
     os.system("./heapDijkstras tests/random/test" + str(i) + "_random >> heaptimes_random.txt")
 
 # lazy heap
+print("Running lazyheapDijkstras...")
 for i in range(5,501):
-    if (!path.exists("tests/sparse/test" + str(i) + "_sparse"))
+    if (not os.path.exists("tests/sparse/test" + str(i) + "_sparse")):
         continue
-
+    print(i)
+    #print("Running lazyheapDijkstras sparse...");
     os.system("./lazyheapDijkstras tests/sparse/test" + str(i) + "_sparse >> heaptimes_sparse.txt")
 
+    #print("Running lazyheapDijkstras dense...");
     os.system("./lazyheapDijkstras tests/dense/test" + str(i) + "_dense >> lazyheaptimes_dense.txt")
 
+    #print("Running lazyheapDijkstras random...");
     os.system("./lazyheapDijkstras tests/random/test" + str(i) + "_random >> lazyheaptimes_random.txt")
+
+print("SCRIPT COMPLETE");
 
