0784266beeae44c7370ae950a95ebf9233780d6d
Nicholas Stecyna <nstecyn1@binghamton.edu>
2020-05-04 00:01:46 -0400

    updated script

--- script.py
+++ script.py
@@ -6,35 +6,47 @@ import os
 
 os.system("break>matrixtimes.txt")
 
-for i in range(5,500):
-	os.system("echo '\n" + str(i) + " vertices sparce graph:' >> matrixtimes_sparce.txt")
-	os.system("./matrixDijkstras tests/sparse/test" + str(i) + "_sparse >> matrixtimes_sparce.txt")
+for i in range(5,501):
+    if (!path.exists("tests/sparse/test" + str(i) + "_sparse"))
+        continue
+    os.system("./matrixDijkstras tests/sparse/test" + str(i) + "_sparse >> matrixtimes_spare.txt")
 
-	os.system("echo '\n" + str(i) + " vertices dense graph:' >> matrixtimes_dense.txt")
-	os.system("./matrixDijkstras tests/dense/test" + str(i) + "_dense >> matrixtimes_dense.txt")
+    os.system("./matrixDijkstras tests/dense/test" + str(i) + "_dense >> matrixtimes_dense.txt")
 
-	os.system("echo '\n" + str(i) + " vertices random graph:' >> matrixtimes_random.txt")
-	os.system("./matrixDijkstras tests/random/test" + str(i) + "_random >> matrixtimes_random.txt")
+    os.system("./matrixDijkstras tests/random/test" + str(i) + "_random >> matrixtimes_random.txt")
+
+'''
+m_sparce_avg = 0
+count = 0
+
+open('myfile.txt', 'r') 
+lines = file1.readlines() 
+  
+for l in lines: 
+    linelist = l.strip(",")
+    m_sparse_avg += linelist[1]
+    count++
+'''
 
 # heap
-for i in range(5,500):
-	os.system("echo '\n" + str(i) + " vertices sparce graph:' >> heaptimes_sparce.txt")
-	os.system("./heapDijkstras tests/sparse/test" + str(i) + "_sparse >> heaptimes_sparce.txt")
+for i in range(5,501):
+    if (!path.exists("tests/sparse/test" + str(i) + "_sparse"))
+        continue
 
-	os.system("echo '\n" + str(i) + " vertices dense graph:' >> heaptimes_dense.txt")
-	os.system("./heapDijkstras tests/dense/test" + str(i) + "_dense >> heaptimes_dense.txt")
+    os.system("./heapDijkstras tests/sparse/test" + str(i) + "_sparse >> heaptimes_sparse.txt")
 
-	os.system("echo '\n" + str(i) + " vertices random graph:' >> heaptimes_random.txt")
-	os.system("./heapDijkstras tests/random/test" + str(i) + "_random >> heaptimes_random.txt")
+    os.system("./heapDijkstras tests/dense/test" + str(i) + "_dense >> heaptimes_dense.txt")
+
+    os.system("./heapDijkstras tests/random/test" + str(i) + "_random >> heaptimes_random.txt")
 
 # lazy heap
-for i in range(5,500):
-	os.system("echo '\n" + str(i) + " vertices sparce graph:' >> lazyheaptimes_sparce.txt")
-	os.system("./lazyheapDijkstras tests/sparse/test" + str(i) + "_sparse >> heaptimes_sparce.txt")
+for i in range(5,501):
+    if (!path.exists("tests/sparse/test" + str(i) + "_sparse"))
+        continue
 
-	os.system("echo '\n" + str(i) + " vertices dense graph:' >> lazyheaptimes_dense.txt")
-	os.system("./lazyheapDijkstras tests/dense/test" + str(i) + "_dense >> lazyheaptimes_dense.txt")
+    os.system("./lazyheapDijkstras tests/sparse/test" + str(i) + "_sparse >> heaptimes_sparse.txt")
 
-	os.system("echo '\n" + str(i) + " vertices random graph:' >> lazyheaptimes_random.txt")
-	os.system("./lazyheapDijkstras tests/random/test" + str(i) + "_random >> lazyheaptimes_random.txt")
+    os.system("./lazyheapDijkstras tests/dense/test" + str(i) + "_dense >> lazyheaptimes_dense.txt")
+
+    os.system("./lazyheapDijkstras tests/random/test" + str(i) + "_random >> lazyheaptimes_random.txt")
 
